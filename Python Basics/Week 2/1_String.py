9f18252ac8d1f0ade00961c0192558d3d49b50f9
DhinakaranSelvaraj <code.dhinakaran@gmail.com>
2025-06-02 22:10:12 +0530

    Learned and Practice about String

---
+++ Python Basics/Week 2/1_String.py
@@ -0,0 +1,19 @@
+str = "dhinakaranselvaraj"
+str1 = "QA"
+str2 = "dhina"
+
+print(str[1])
+print(str[0:5])
+print(str+str1)
+print(str2 in str)
+
+var = str.split("k")
+print(var)
+print(var[0])
+
+str4 = "great "
+str5 = " great"
+str6 = " great "
+print(str6.strip())
+print(str4.rstrip())
+print(str5.lstrip())
